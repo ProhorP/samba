82650a1ea32f1860915bfd8b717b3d940c80625c
Evgeny Sinelnikov <sin@altlinux.ru>
2017-10-12 05:21:27 +0300

    s4-dsdb, s4-kdc, s4-rpc_server: Logging and avoid TGS request problem with forest trust information for crossRef class object nCName attribute

--- source4/kdc/db-glue.c
+++ source4/kdc/db-glue.c
@@ -1670,6 +1670,7 @@ static krb5_error_code samba_kdc_fetch_client(krb5_context context,
 				      mem_ctx, principal, user_attrs,
 				      &realm_dn, &msg);
 	if (ret != 0) {
+		DEBUG(5, ("samba_kdc_fetch_client: failed for samba_kdc_lookup_client()\n"));
 		return ret;
 	}
 
@@ -1677,6 +1678,10 @@ static krb5_error_code samba_kdc_fetch_client(krb5_context context,
 				      principal, SAMBA_KDC_ENT_TYPE_CLIENT,
 				      flags,
 				      realm_dn, msg, entry_ex);
+	if (ret != 0) {
+		krb5_warnx(context, "samba_kdc_fetch_client: message2entry failed");
+	}
+
 	return ret;
 }
 
@@ -2012,6 +2017,7 @@ static krb5_error_code samba_kdc_fetch_server(krb5_context context,
 	ret = samba_kdc_lookup_server(context, kdc_db_ctx, mem_ctx, principal,
 				      flags, server_attrs, &realm_dn, &msg);
 	if (ret != 0) {
+		DEBUG(5, ("samba_kdc_fetch_server: failed for samba_kdc_lookup_server()\n"));
 		return ret;
 	}
 
@@ -2020,7 +2026,7 @@ static krb5_error_code samba_kdc_fetch_server(krb5_context context,
 				      flags,
 				      realm_dn, msg, entry_ex);
 	if (ret != 0) {
-		krb5_warnx(context, "samba_kdc_fetch: message2entry failed");
+		krb5_warnx(context, "samba_kdc_fetch_server: message2entry failed");
 	}
 
 	return ret;
@@ -2174,6 +2180,7 @@ static krb5_error_code samba_kdc_lookup_realm(krb5_context context,
 	status = dsdb_trust_routing_table_load(kdc_db_ctx->samdb,
 					       frame, &trt);
 	if (!NT_STATUS_IS_OK(status)) {
+		DEBUG(3, ("samba_kdc_lookup_realm: dsdb_trust_routing_table_load() failed: %s\n", nt_errstr(status)));
 		TALLOC_FREE(frame);
 		return EINVAL;
 	}
