662afafd74119fab1ef14653c6e9fd4a7d1da51b
Evgeny Sinelnikov <sin@altlinux.org>
2019-07-22 19:09:18 +0400

    Set parameter limits the maximum number of smbd processes to 768

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -632,7 +632,7 @@ static void init_globals(struct loadparm_context *lp_ctx, bool reinit_globals)
 	Globals.max_mux = 50;	/* This is *needed* for profile support. */
 	Globals.lpq_cache_time = 30;	/* changed to handle large print servers better -- jerry */
 	Globals._disable_spoolss = false;
-	Globals.max_smbd_processes = 0;/* no limit specified */
+	Globals.max_smbd_processes = 768;
 	Globals.smbd_read_timeout = 3000;
 	Globals.username_level = 0;
 	Globals.deadtime = 0;
