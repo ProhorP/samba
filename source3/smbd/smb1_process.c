026c4e212108f73684de281c4de9f044b12d2475
Evgeny Sinelnikov <sin@altlinux.org>
2019-07-22 19:05:35 +0400

    Add smbd read timeout parameter defines smbd read socket timeout

--- source3/smbd/smb1_process.c
+++ source3/smbd/smb1_process.c
@@ -2029,6 +2029,7 @@ void smbd_smb1_server_connection_read_handler(struct smbXsrv_connection *xconn,
 	NTSTATUS status;
 	uint32_t seqnum;
 
+	unsigned int timeout = lp_smbd_read_timeout();
 	bool async_echo = lp_async_smb_echo_handler();
 	bool from_client = false;
 
@@ -2058,7 +2059,7 @@ void smbd_smb1_server_connection_read_handler(struct smbXsrv_connection *xconn,
 	/* TODO: make this completely nonblocking */
 	status = receive_smb_talloc(mem_ctx, xconn, fd,
 				    (char **)(void *)&inbuf,
-				    0, /* timeout */
+				    timeout,
 				    &unread_bytes,
 				    &encrypted,
 				    &inbuf_len, &seqnum,
