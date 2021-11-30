e0da3d5109b744fe9305a5a35374797ab029e92e
Evgeny Sinelnikov <sin@altlinux.org>
2021-11-30 23:43:11 +0400

    loadparm: Set parameter "min domain uid" deafult value to 500.
    Due minimal user uid in ALT Systems starts from 500 paramter "min domain uid"
    changes to its value.

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -3150,7 +3150,7 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx,
 				  "min domain uid",
-				  "1000");
+				  "500");
 
 	lpcfg_do_global_parameter(lp_ctx,
 				  "rpc start on demand helpers",
