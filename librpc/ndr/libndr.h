1fb20b1ea00ad34cdf0171112daca5436765ba60
Evgeny Sinelnikov <sin@altlinux.org>
2024-05-10 20:43:31 +0400

    librpc:ndr: add inttypes.h to libndr.h for separate build support.

--- librpc/ndr/libndr.h
+++ librpc/ndr/libndr.h
@@ -26,6 +26,7 @@
 #define __LIBNDR_H__
 
 #include <talloc.h>
+#include <inttypes.h>
 #include "../lib/util/discard.h" /* for discard_const */
 #include "../lib/util/data_blob.h"
 #include "../lib/util/time.h"
